82bd21e9bd1fee656a5dec897a047ff7434fa7c6
tishirt <danielnjui161@gmail.com>
2023-03-16 15:30:57 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -A program that prints a line using printf followd with a new line
+ * Return: 0 (succes)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+	return (0);
+}
